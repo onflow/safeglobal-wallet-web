b8e5d91af075a647cb3ff39f1b6ade7eb832fa3b
ajimeno04 <jimenobeltran11@gmail.com>
2025-02-18 13:13:42 +0100

    chore: Add react-markdown dependency

--- apps/web/src/components/common/Footer/index.tsx
+++ apps/web/src/components/common/Footer/index.tsx
@@ -26,7 +26,7 @@ const FooterLink = ({ children, href }: { children: ReactNode; href: string }): 
 
 const Footer = (): ReactElement | null => {
   const router = useRouter()
-  const isOfficialHost = useIsOfficialHost()
+  //const isOfficialHost = useIsOfficialHost()
 
   if (!footerPages.some((path) => router.pathname.startsWith(path))) {
     return null
