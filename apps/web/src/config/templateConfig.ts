8220d38fd64d9673e1dcc76d9a27c9735515d784
msvstj <savastijonok@gmail.com>
2026-05-08 13:00:17 +0300

    feat(network-config): add flow network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,26 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/flow/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  EXTRA_FOOTER_LINKS: [
+    { label: 'X', link: 'https://x.com/flow_blockchain' },
+    { label: 'Discord', link: 'https://discord.com/invite/J6fFnh2xx6' },
+  ],
+  IS_LICENSED: true,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '24px',
+    },
+    WELCOME: {
+      W: '120px',
+      H: '64px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
